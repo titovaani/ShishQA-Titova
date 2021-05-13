60e62e63428e29072cdc8f3181cc416e3e365890
titovaani <antit1646@gmail.com>
2021-05-13 11:14:58 +0700

    pr3

---
+++ practice-3/src/test/java/TestTwo.java
@@ -0,0 +1,53 @@
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class TestTwo{
+    private WebDriver driver;
+    private String baseUrl;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "src\\chromedriver.exe");
+        driver = new ChromeDriver();
+        baseUrl = "https://sandbox.cardpay.com/MI/cardpayment2.html?orderXml=PE9SREVSIFdBTExFVF9JRD0nODI5OScgT1JERVJfTlVNQkVSPSc0NTgyMTEnIEFNT1VOVD0nMjkxLjg2JyBDVVJSRU5DWT0nRVVSJyAgRU1BSUw9J2N1c3RvbWVyQGV4YW1wbGUuY29tJz4KPEFERFJFU1MgQ09VTlRSWT0nVVNBJyBTVEFURT0nTlknIFpJUD0nMTAwMDEnIENJVFk9J05ZJyBTVFJFRVQ9JzY3NyBTVFJFRVQnIFBIT05FPSc4NzY5OTA5MCcgVFlQRT0nQklMTElORycvPgo8L09SREVSPg==&sha512=998150a2b27484b776a1628bfe7505a9cb430f276dfa35b14315c1c8f03381a90490f6608f0dcff789273e05926cd782e1bb941418a9673f43c47595aa7b8b0d";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().window().fullscreen();
+    }
+
+    @Test
+    public void testTets4() {
+        driver.get(baseUrl);
+        driver.findElement(By.id("input-card-number")).click();
+        driver.findElement(By.id("input-card-number")).clear();
+        driver.findElement(By.id("input-card-number")).sendKeys("5555 5555 5555 4444");
+        driver.findElement(By.id("input-card-holder")).click();
+        driver.findElement(By.id("input-card-holder")).clear();
+        driver.findElement(By.id("input-card-holder")).sendKeys("FER WERC");
+        driver.findElement(By.id("card-expires-month")).click();
+        new Select(driver.findElement(By.id("card-expires-month"))).selectByVisibleText("04");
+        driver.findElement(By.id("card-expires-month")).click();
+        driver.findElement(By.id("card-expires-year")).click();
+        new Select(driver.findElement(By.id("card-expires-year"))).selectByVisibleText("2029");
+        driver.findElement(By.id("card-expires-year")).click();
+        driver.findElement(By.id("input-card-cvc")).click();
+        driver.findElement(By.id("input-card-cvc")).clear();
+        driver.findElement(By.id("input-card-cvc")).sendKeys("456");
+        driver.findElement(By.id("action-submit")).click();
+        driver.findElement(By.id("success")).click();
+        driver.findElement(By.id("payment-status")).click();
+        driver.findElement(By.id("payment-item-status")).click();
+        assertEquals("Payment status", driver.findElement(By.xpath("//div[@id='payment-item-status']/div/span")).getText());
+    }
+
+    
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+}
+
